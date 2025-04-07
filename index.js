3eec7b6cbf7d658e0e7330e424847cd88167b91d
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-06 19:03:49 -0500

    Connected with DB successfully, rendered data

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const pokemonList = document.querySelector("#pokemonList")
+const allPokeDiv = document.querySelector("#allPoke")
 
 let pokemons = []
 
-const fetchPokemons = async () => {
-    const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
-    const data = await response.json()
-    console.log(data)
+
+const render = () => {
+    const html = pokemons.map((poke)=>{
+        console.log(poke.name, poke.url)
+        return `<a href=#${poke.name}>${poke.name}</a>`
+    })
+    allPokeDiv.innerHTML = html.join("")
 }
 
-fetchPokemons()
+const fetchAllPokemons = async () => {
+    try {
+        const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+        const data = await response.json()
+        pokemons = data.results
+        console.log(pokemons)
+        render()
+    } catch (error) {
+        
+    }
+}
+
+fetchAllPokemons()
