258bd6412aafdafcbc8d26f702f5374ef3f4ea81
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-06 19:55:16 -0500

    Added SinglePoke result

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const allPokeDiv = document.querySelector("#allPoke")
+const singlePokeDiv = document.querySelector("#singlePoke")
 
 let pokemons = []
 
+window.addEventListener("hashchange", (event) => {
+    render()
+})
 
-const render = () => {
+const render = async () => {
     const html = pokemons.map((poke)=>{
-        console.log(poke.name, poke.url)
+        // console.log(poke.name, poke.url)
         return `<a href=#${poke.name}>${poke.name}</a>`
     })
     allPokeDiv.innerHTML = html.join("")
+
+    const name = window.location.hash.slice(1)
+    console.log(name)
+
+    const singlePoke = pokemons.find((poke) => {
+        return poke.name === name
+    })
+    console.log(singlePoke)
+
+    if(singlePoke){
+        const pokeData = await fetch(singlePoke.url)
+        const singlePokeData = await pokeData.json()
+        console.log(singlePokeData)
+
+        singlePokeDiv.innerHTML = `
+            <h2>Selected Pokemon</h2>
+            <h2>${singlePokeData.name}</h2>
+            <img src=${singlePokeData.sprites.front_default} />
+        `
+
+    }
 }
 
 const fetchAllPokemons = async () => {
@@ -16,7 +41,7 @@ const fetchAllPokemons = async () => {
         const response = await fetch ("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
         const data = await response.json()
         pokemons = data.results
-        console.log(pokemons)
+        // console.log(pokemons)
         render()
     } catch (error) {
         
